c1783962a564328e411bf19d3410af823db1264f
Erhan <teacherhan@gmail.com>
2020-11-16 19:55:55 -0800

    Update basicMovements.c

--- basicMovements.c
+++ basicMovements.c
@@ -1,4 +1,10 @@
 #pragma config(StandardModel, "Clawbot IQ With Sensors")
+void goBack(float dist){
+	setMotor(leftMotor,50);
+	setMotor(rightMotor,50);
+	waitUntil(getdistanceValue(distanceMM)>dist);
+	stopMultipleMotors(leftMotor, rightMotor);
+}
 
 task main()
 {
